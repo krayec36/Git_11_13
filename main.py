13e5d1d73067f2427c812bc52f4c1d3eaabb86ff
krayec36 <maciejkrajewski36@proton.me>
2024-12-11 20:42:16 +0100

    Wersja alpha

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello world")
+import os
 
-for i in range(5):
-    print(i)
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+name = input("Podaj swoje imię... ")
+
+clear_console()
+print(f"Witaj {name}, twoje imię ma {len(name)} liter")
